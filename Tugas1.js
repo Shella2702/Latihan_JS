a3845932bda1c183c3a8e8f9b4a37f466f2c68f4
Shella Batrisyia L <62591363+Shella2702@users.noreply.github.com>
2021-07-03 12:50:31 +0700

    Add files via upload

---
+++ Tugas1.js
@@ -0,0 +1,18 @@
+// Soal No 1//
+
+let jabatan = 'CTO'
+
+if (jabatan = 'CTO') {
+    console.log("Jika saya sebagai CTO, maka tugas saya sebagai CTO adalah mengelola pengembangan dan menyatukan pengembangan");
+} else if (jabatan = 'CEO') {
+    console.log("jika saya sebagai CEO, Maka tugas saya sebagai CEO adalah merancang dan memprediksi tren pasar");
+} else if (jabatan = 'HR') {
+    console.log("Jika sya sebagai HR, maka tugas saya sebagai HR adalah perekrutran karyawan ");
+} else if (jabatan = 'PROGRAMER') {
+    console.log("Jika saya sebagai Programmer, maka tugas saya sebagai programmer adalah merancang dan membuat website maupun desain website ");
+} else {
+    console.log("tidak ada lowongan Perusahaan Startup yang tersedia...");
+};
+
+console.log(jabatan = "CTO")
+
---
+++ Tugas4.js
@@ -0,0 +1,11 @@
+//tugas No 4//
+
+let arah = "UP";
+
+switch(arah){
+    case "UP" : {console.log("Karakter berjalan ke atas."); break;}
+    case "DOWN" : {console.log("Karakter berjalan ke bawah."); break;}
+    case "LEFT" : {console.log("Karakter berjalan ke kiri."); break;}
+    case "RIGHT" : {console.log("Karakter berjalan ke kanan."); break;}
+    default: {console.log("Karakter diam di tempat."); break;}
+};
