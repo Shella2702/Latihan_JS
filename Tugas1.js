a06643cb5f28939032c0e4e8c16ebdf9895c01eb
Shella Batrisyia L <62591363+Shella2702@users.noreply.github.com>
2021-08-10 15:58:46 +0700

    Update Tugas1.js

--- Tugas1.js
+++ Tugas1.js
@@ -4,11 +4,11 @@ let jabatan = 'CTO'
 
 if (jabatan = 'CTO') {
     console.log("Jika saya sebagai CTO, maka tugas saya sebagai CTO adalah mengelola pengembangan dan menyatukan pengembangan");
-} else if (jabatan = 'CEO') {
+} else if (jabatan == 'CEO') {
     console.log("jika saya sebagai CEO, Maka tugas saya sebagai CEO adalah merancang dan memprediksi tren pasar");
-} else if (jabatan = 'HR') {
+} else if (jabatan == 'HR') {
     console.log("Jika sya sebagai HR, maka tugas saya sebagai HR adalah perekrutran karyawan ");
-} else if (jabatan = 'PROGRAMER') {
+} else if (jabatan == 'PROGRAMER') {
     console.log("Jika saya sebagai Programmer, maka tugas saya sebagai programmer adalah merancang dan membuat website maupun desain website ");
 } else {
     console.log("tidak ada lowongan Perusahaan Startup yang tersedia...");
